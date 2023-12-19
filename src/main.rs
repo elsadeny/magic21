d9a217ee7466a3a1cb90ab9a65e9445d2a6de886
elsadeny <alphasadeny@gmail.com>
2023-12-19 23:00:21 +0200

    second touch

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
-use std:: ops::Index;
+pub mod test;
 
+use std::{io,ops::Index};
+use rand::Rng;
 
 #[derive(Debug)]
-struct Nmagic {
-    numbezi : Vec<Vec<i16>>
+struct Magic21 {
+    numbers_21 : Vec<Vec<i16>>
 }
 
-impl Nmagic {
-    fn new() -> Nmagic {
-        let ix : Vec<i16> = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21].to_vec();
-        Nmagic{
-            numbezi :  match_mix(ix),
+impl Magic21 {
+    fn new() -> Magic21 {
+        // Create a vector to store the random numbers
+        let mut random_numbers: Vec<Vec<i16>> = Vec::new();
+
+         // Generate 21 random numbers and push them into the vector
+        let mut rng = rand::thread_rng();
+        for _ in 0..3 {
+            let inner_vec: Vec<i16> = (0..7).map(|_| rng.gen_range(0..100)).collect();
+            random_numbers.push(inner_vec);
+        }
+        Magic21{
+            numbers_21 : random_numbers,
         }
     }
-    fn disp(&self) {
-        for(i,item) in self.numbezi.iter().enumerate(){
-            println!("GROUP {} = {:?}",i,item);
+    fn display(&self) {
+        for(i,item) in self.numbers_21.iter().enumerate(){
+            println!("GROUP {} = {:?}",(i+1),item);
         }
     }
     
     fn process_choice(&mut self,choice:u8) {
-        let vx = self.numbezi.clone();
+        let vx = self.numbers_21.clone();
     
         let vxn = match choice {
             1 => [vx.index(1).clone(),vx.index(0).clone(),vx.index(2).clone()].to_vec(),
-            3 =>  [vx.index(1).clone(),vx.index(2).clone(),vx.index(0).clone()].to_vec(),
+            3 =>  [vx.index(0).clone(),vx.index(2).clone(),vx.index(1).clone()].to_vec(),
             _ => vx.to_vec()
         };
-        self.numbezi = vxn;
+        self.numbers_21 = vxn;
     }
 
-    fn magic_mix(&mut self) {
+    fn magic_shuffle(&mut self) {
         let mut mx1:Vec<i16> = vec![];
         let mut mx2:Vec<i16> = vec![];
         let mut mx3:Vec<i16> = vec![];
-        let i  : Vec<i16>= self.numbezi.clone().iter().flat_map(|inner|{
+        let i  : Vec<i16>= self.numbers_21.clone().iter().flat_map(|inner|{
             inner.iter().cloned()
         }).collect();
 
@@ -45,42 +55,27 @@ impl Nmagic {
                 _ =>[mx3.clone(),[item.to_owned()].to_vec()].concat().clone_into(&mut mx3)
             }
         }
-        self.numbezi = [mx1.to_owned(),mx2.to_owned(),mx3.to_owned()].to_vec();
+        self.numbers_21 = [mx1.to_owned(),mx2.to_owned(),mx3.to_owned()].to_vec();
     }
-}
-
-
-fn match_mix(sequence : Vec<i16>) -> Vec<Vec<i16>>{
-
-    let mut mx1:Vec<i16> = vec![];
-    let mut mx2:Vec<i16> = vec![];
-    let mut mx3:Vec<i16> = vec![];
-
-    let xp = sequence.clone();
-        
-    if sequence.clone().len() == 21 {
-        let _ = xp.iter().for_each(|f|{
-            match (f.to_owned())%3 {
-                1 => [mx1.clone(),[f.to_owned()].to_vec()].concat().clone_into(&mut mx1),
-                2 => [mx2.clone(),[f.to_owned()].to_vec()].concat().clone_into(&mut mx2),
-                _ =>[mx3.clone(),[f.to_owned()].to_vec()].concat().clone_into(&mut mx3)
-            }
-        });
-    } else{
-        println!("vector size is not 21 instread is {}",sequence.clone().len())
-    }
-
-   return [mx1.to_owned(),mx2.to_owned(),mx3.to_owned()].to_vec();
-
-    
 }
 
 fn main() {
-    //let ix : Vec<i16> = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21].to_vec();
-    println!("Think a number between 1 and 21 and  answer 3 few questions");
-    println!("which side is the number located? A,B or C");
-    let mut pi = Nmagic::new();
-    
-    pi.disp();
+    println!("Think of a number between 1 and 21 and  answer 3 few questions");
+    println!("");
+    let mut magic21 = Magic21::new();
+    (0..=2).for_each(|_|{
+        magic21.display();
+        println!("Which Group is the number located? 1,2 or 3");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("could not read your choice");
+        if let Ok(choice) = input.trim().parse::<u8>(){
+            magic21.process_choice(choice)
+        }else{
+            println!("\nplease select numbers only");
+        }
+        magic21.magic_shuffle();
+    });
+
+    println!("WoWoo the magic number you are thinking is {}",magic21.numbers_21.index(1).index(3));
 
 }
--- src/test.rs
+++ src/test.rs
@@ -1,16 +1,29 @@
-
+//src/test.rs
 #[cfg(test)]
 mod tests{
-
-use super::*;
+use std::ops::Index;
+use crate::Magic21;
 
     #[test]
-    fn test_addition() {
-        assert_eq!(2+2,4);
+    fn test_new_magic21(){
+        let magic21 = Magic21::new();
+        assert_eq!(magic21.numbers_21, [[1, 4, 7, 10, 13, 16, 19],[2, 5, 8, 11, 14, 17, 20], [3, 6, 9, 12, 15, 18, 21]].to_vec())
     }
 
     #[test]
-    fn test_subtraction() {
-        assert_eq!(5 - 3, 2);
+    fn test_process_choice(){
+        let mut magic21 = Magic21::new();
+        magic21.process_choice(3);
+        assert_eq!(magic21.numbers_21,[[1, 4, 7, 10, 13, 16, 19],[3, 6, 9, 12, 15, 18, 21],[2, 5, 8, 11, 14, 17, 20]].to_vec())
     }
+
+    #[test]
+    fn test_magic_mix(){
+        let mut magic21 = Magic21::new();
+        magic21.process_choice(3);
+        assert_eq!(magic21.numbers_21,[[1, 4, 7, 10, 13, 16, 19],[3, 6, 9, 12, 15, 18, 21],[2, 5, 8, 11, 14, 17, 20]].to_vec());
+        magic21.magic_shuffle();
+        assert_eq!(magic21.numbers_21.index(0).to_owned(),[1, 10, 19, 9, 18, 5, 14].to_vec());
+    
+       }
 }
